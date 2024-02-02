d69a94ec2be768f3029c7f9c3aa2dde2c3ae0d0d
serhiy-petryk <sergei_petrik@yahoo.com>
2024-02-02 04:54:58 +0200

    added StockAnalysis actions (not finished)

---
+++ Quote2024/Data/Actions/StockAnaysis/StockAnalysisActions.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO.Compression;
+using System.Linq;
+using Data.Helpers;
+using Data.Models;
+
+namespace Data.Actions.StockAnaysis
+{
+    public class StockAnalysisActions
+    {
+        private const string Url = @"https://stockanalysis.com/actions/";
+        private const string Folder = @"E:\Quote\WebData\Splits\StockAnalysis\Actions\";
+
+        public static void Start()
+        {
+            Logger.AddMessage($"Started");
+
+            var timeStamp = CsUtils.GetTimeStamp();
+            var zipFileName = Folder + $"StockAnalysisActions_{timeStamp.Item2}.zip";
+
+            // Download html data
+            var o = Download.DownloadToBytes(Url, false);
+            if (o is Exception ex)
+                throw new Exception($"StockAnalysisActions: Error while download from {Url}. Error message: {ex.Message}");
+
+            // Save html data to zip
+            var entry = new VirtualFileEntry($"StockAnalysisActions_{timeStamp.Item2}.html", (byte[]) o);
+            ZipUtils.ZipVirtualFileEntries(zipFileName, new[] { entry });
+
+            // Parse and save to database
+            var itemCount = ParseAndSaveToDb(zipFileName);
+
+            Logger.AddMessage($"!Finished. Items: {itemCount:N0}. Zip file size: {CsUtils.GetFileSizeInKB(zipFileName):N0}KB. Filename: {zipFileName}");
+        }
+
+        public static int ParseAndSaveToDb(string zipFileName)
+        {
+            var itemCount = 0;
+            using (var zip = ZipFile.Open(zipFileName, ZipArchiveMode.Read))
+                foreach (var entry in zip.Entries.Where(a => a.Length > 0))
+                {
+                    var items = new List<Models.ActionStockAnalysis>();
+                    Parse(entry.GetContentOfZipEntry(), items, entry.LastWriteTime.DateTime);
+                    itemCount += items.Count;
+                    // Save data to database
+                    if (items.Count > 0)
+                    {
+                        DbUtils.ClearAndSaveToDbTable(items.Where(a => !a.IsBad), "dbQ2023Others..Bfr_ActionsStockAnalysis",
+                            "Date", "Type", "Symbol", "OtherSymbolOrName", "Name", "Description", "SplitRatio", "SplitK",
+                            "TimeStamp");
+
+                        DbUtils.ClearAndSaveToDbTable(items.Where(a => a.IsBad),
+                            "dbQ2023Others..Bfr_ActionsStockAnalysisError", "Date", "Type", "Symbol", "OtherSymbolOrName",
+                            "Description", "TimeStamp");
+
+                        DbUtils.RunProcedure("dbQ2023Others..pUpdateActionsStockAnalysis");
+                    }
+                }
+
+            return itemCount;
+        }
+
+        #region ==========  Private section  ==========
+        private static void Parse(string content, List<Models.ActionStockAnalysis> items, DateTime fileTimeStamp)
+        {
+            if (!TryToParseAsJson(content, items, fileTimeStamp))
+                ParseAsHtml(content, items, fileTimeStamp);
+        }
+
+        private static bool TryToParseAsJson(string content, List<Models.ActionStockAnalysis> items, DateTime fileTimeStamp)
+        {
+            var i1 = content.IndexOf("const data =", StringComparison.InvariantCulture);
+            if (i1 == -1) return false;
+            var i2 = content.IndexOf("}}]", i1 + 12, StringComparison.InvariantCulture);
+            var s = content.Substring(i1 + 12, i2 - i1 - 12 + 3).Trim();
+            var i12 = s.IndexOf("{\"type\":", StringComparison.InvariantCulture);
+            i12 = s.IndexOf("{\"type\":", i12 + 8, StringComparison.InvariantCulture);
+            var s2 = s.Substring(i12, s.Length - i12 - 1);
+
+            var oo = ZipUtils.DeserializeJson<cRoot>(s2);
+            foreach (var item in oo.data.data)
+                items.Add(new ActionStockAnalysis(item, fileTimeStamp));
+
+            return true;
+        }
+
+        private static void ParseAsHtml(string content, List<Models.ActionStockAnalysis> items, DateTime fileTimeStamp)
+        {
+            var i1 = content.IndexOf(">Action</th>", StringComparison.InvariantCultureIgnoreCase);
+            if (i1 == -1)
+                throw new Exception("Check StockAnalysis.WebArchiveActions parser");
+            var i2 = content.IndexOf("</tbody>", i1 + 12, StringComparison.InvariantCultureIgnoreCase);
+
+            var rows = content.Substring(i1 + 12, i2 - i1 - 12).Replace("</thead>", "").Replace("<tbody>", "")
+                .Replace("</tr>", "").Replace("<!-- HTML_TAG_START -->", "").Replace("<!-- HTML_TAG_END -->", "")
+                .Split(new[] { "<tr>", "<tr " }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var row in rows)
+            {
+                if (string.IsNullOrWhiteSpace(row)) continue;
+                var item = new Data.Models.ActionStockAnalysis(row.Trim(), fileTimeStamp);
+                items.Add(item);
+            }
+        }
+        #endregion
+
+        #region =======  Json subclasses  =============
+        private class cRoot
+        {
+            public string type;
+            public cData data;
+        }
+        private class cData
+        {
+            public string action;
+            public string type;
+            public object props;
+            public cItem[] data;
+            public int fullCount;
+        }
+        public class cItem
+        {
+            public string date;
+            public string type;
+            public string symbol;
+            public string name;
+            public string other;
+            public string text;
+
+            public DateTime pDate => DateTime.Parse(date, CultureInfo.InvariantCulture);
+            public string pSymbol => symbol.StartsWith("$") ? symbol.Substring(1) : symbol;
+            public string pOther => other == "N/A" || string.IsNullOrEmpty(other) ? null : other;
+            public string pName => string.IsNullOrEmpty(name) ? null : name;
+
+            public override string ToString()
+            {
+                return $"{pDate:yyyy-MM-dd}, {pSymbol}, {type}, {pOther}, {text}";
+            }
+        }
+        #endregion
+    }
+}
---
+++ Quote2024/Data/Actions/StockAnaysis/StockAnalysisIPOs.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.IO.Compression;
+using System.Linq;
+using Data.Helpers;
+
+namespace Data.Actions.StockAnaysis
+{
+    public class StockAnalysisIPOs
+    {
+        private const string Url = @"https://stockanalysis.com/api/screener/s/f?m=ipoDate&s=desc&c=ipoDate,s,n,ipoPrice,ippc,exchange,sector,industry,employees&cn=5000&i=histip";
+        private const string Folder = @"E:\Quote\WebData\Splits\StockAnalysis\IPOs\";
+
+        public static void Start()
+        {
+            Logger.AddMessage($"Started");
+
+            var timeStamp = CsUtils.GetTimeStamp();
+            var zipFileName = Folder + $"StockAnalysisIPOs_{timeStamp.Item2}.zip";
+
+            // Download html data
+            var o = Download.DownloadToBytes(Url, true);
+            if (o is Exception ex)
+                throw new Exception($"StockAnalysisIPOs: Error while download from {Url}. Error message: {ex.Message}");
+
+            // Save html data to zip
+            var entry = new VirtualFileEntry($"StockAnalysisIPOs_{timeStamp.Item2}.json", (byte[])o);
+            ZipUtils.ZipVirtualFileEntries(zipFileName, new[] { entry });
+
+            // Parse and save to database
+            var itemCount = ParseAndSaveToDb(zipFileName);
+
+            Logger.AddMessage($"!Finished. Items: {itemCount:N0}. Zip file size: {CsUtils.GetFileSizeInKB(zipFileName):N0}KB. Filename: {zipFileName}");
+        }
+
+        public static int ParseAndSaveToDb(string zipFileName)
+        {
+            var itemCount = 0;
+            using (var zip = ZipFile.Open(zipFileName, ZipArchiveMode.Read))
+                foreach (var entry in zip.Entries.Where(a => a.Length > 0))
+                {
+                    var oo = ZipUtils.DeserializeJson<cRoot>(entry);
+                    foreach (var item in oo.data.data)
+                        item.TimeStamp = entry.LastWriteTime.DateTime;
+                    itemCount += oo.data.data.Length;
+
+                    // Save data to database
+                    if (oo.data.data.Length > 0)
+                    {
+                        DbUtils.ClearAndSaveToDbTable(oo.data.data, "dbQ2023Others..Bfr_IpoStockAnalysis", "Symbol", "Date", "Exchange",
+                            "Name", "IpoPrice", "CurrentPrice", "Sector", "Industry", "employees", "TimeStamp");
+
+                        DbUtils.RunProcedure("dbQ2023Others..pUpdateIpoStockAnalysis");
+                    }
+                }
+
+            return itemCount;
+        }
+
+        #region =======  Json subclasses  =============
+        private class cRoot
+        {
+            public int status;
+            public cData data;
+        }
+
+        private class cData
+        {
+            public int resultsCount;
+            public cItem[] data;
+        }
+        private class cItem
+        {
+            public string s;
+            public DateTime ipoDate;
+            public string exchange;
+            public string n;
+            public float ipoPrice;
+            public float ippc;
+            public string sector;
+            public string industry;
+            public int? employees;
+
+            public string Symbol => s;
+            public DateTime Date => ipoDate;
+            public string Exchange => exchange;
+            public string Name => n;
+            public float IpoPrice => ipoPrice;
+            public float CurrentPrice => ippc;
+            public string Sector => string.IsNullOrEmpty(sector) ? null : sector;
+            public string Industry => string.IsNullOrEmpty(industry) ? null : industry;
+            public DateTime TimeStamp;
+        }
+        #endregion
+    }
+}
