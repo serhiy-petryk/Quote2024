f13caa71dc72844462ee637ffe072a6bec5d55e7
serhiy-petryk <sergei_petrik@yahoo.com>
2024-02-28 03:55:25 +0200

    added ChartmillScreenerLoader

---
+++ Quote2024/Data/Actions/Chartmill/ChartmillScreenerLoader.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using Data.Helpers;
+
+namespace Data.Actions.Chartmill
+{
+    public class ChartmillScreenerLoader
+    {
+        private const string Url = @"https://www.chartmill.com/chartmill-rest/screener";
+        // private const string parameters2 = @"{""sort"":""ticker"",""sortDir"":""ASC"",""tickers"":null,""conditions"":null,""exchanges"":""125,119,3"",""indexes"":null,""types"":null,""date"":null,""sectors"":""12"",""loadWatchListId"":null,""start"":20}";
+        // private const string parameters = @"{""sort"":""ticker"",""sortDir"":""ASC"",""tickers"":null,""conditions"":null,""exchanges"":""125,119,3"",""indexes"":null,""types"":null,""date"":null,""sectors"":""1,12,36,79,124,146,165,193,216,234,247"",""loadWatchListId"":null,""start"":40}";
+        // at date: {"sort":"ticker","sortDir":"ASC","tickers":null,"conditions":null,"exchanges":"125,119,3","indexes":null,"types":null,"date":"2021-03-05","sectors":"1,12,36,79,124,146,165,193,216,234,247","loadWatchListId":null,"start":20}
+        // all us tickers(10816): {""sort"":""ticker"",""sortDir"":""ASC"",""tickers"":null,""conditions"":null,""exchanges"":""125,119,3"",""indexes"":null,""types"":null,""date"":null,""sectors"":null,""loadWatchListId"":null,""start"":20}
+        // with date: @"{""sort"":""ticker"",""sortDir"":""ASC"",""tickers"":null,""conditions"":null,""exchanges"":""125,119,3"",""indexes"":null,""types"":null,""date"":""2023-01-06"",""sectors"":null,""loadWatchListId"":null,""start"":{0}}";
+        private const string ParameterTemplate = @"{""sort"":""ticker"",""sortDir"":""ASC"",""tickers"":null,""conditions"":null,""exchanges"":""125,119,3"",""indexes"":null,""types"":null,""date"":null,""sectors"":null,""loadWatchListId"":null,""start"":{0}}";
+        private const string ParameterTemplateWithDate = @"{""sort"":""ticker"",""sortDir"":""ASC"",""tickers"":null,""conditions"":null,""exchanges"":""125,119,3"",""indexes"":null,""types"":null,""date"":""{1}"",""sectors"":null,""loadWatchListId"":null,""start"":{0}}";
+        // 2020-01-03,2020-04-03,2020-07-10,2020-10-02,2021-01-08,2021-04-09,2021-07-02,2021-10-01,2022-01-07,2022-01-07,2022-04-01,2022-07-01,2022-10-07,2023-01-06,2023-03-31,2023-07-07,2023-10-02,2024-01-02,
+
+        private const string DataFolder = @"E:\Quote\WebData\Screener\Chartmill\Data";
+        private const string JsonDataFolder = @"E:\Quote\WebData\Screener\Chartmill\Json";
+
+        public static void Start()
+        {
+            Logger.AddMessage($"Started");
+
+            Logger.AddMessage($"Data is downloading ..");
+            var zipFileName = DownloadAndSaveToZip(null);
+
+            // Parse and save data to database
+            Logger.AddMessage($"Parse and save to database");
+            // var zipFileName = @"E:\Quote\WebData\Screener\Chartmill\Data\Chartmill_20240226.zip";
+
+            ParseJsonAndSaveToDb(zipFileName);
+
+            Logger.AddMessage($"!Finished. Items: itemCount:N0. Zip file size: {CsUtils.GetFileSizeInKB(zipFileName):N0}KB. Filename: {zipFileName}");
+        }
+
+        public static void StartHistory()
+        {
+            Logger.AddMessage($"Started");
+
+            var dates =
+                "2020-01-03,2020-04-03,2020-07-10,2020-10-02,2021-01-08,2021-04-09,2021-07-02,2021-10-01,2022-01-07,2022-04-01,2022-07-01,2022-10-07,2023-01-06,2023-03-31,2023-07-07,2023-10-02,2024-01-02"
+                    .Split(',').Select(a => DateTime.ParseExact(a, "yyyy-MM-dd", CultureInfo.InvariantCulture)).ToArray();
+
+            foreach (var date in dates)
+            {
+                Logger.AddMessage($"Data is downloading for {date:yyyy-MM-dd} ..");
+                var zipFileName = DownloadAndSaveToZip(date);
+            }
+            Logger.AddMessage($"!Finished.");
+        }
+
+        public static void ParseAllZipFiles()
+        {
+            Logger.AddMessage($"Started");
+
+            var zipFileNames = Directory.GetFiles(DataFolder, "*.zip");
+            foreach (var zipFileName in zipFileNames)
+            {
+                Logger.AddMessage($"Parse {Path.GetFileName(zipFileName)}");
+                ParseJsonAndSaveToDb(zipFileName);
+            }
+            Logger.AddMessage($"!Finished.");
+        }
+
+        private static void ParseJsonAndSaveToDb(string zipFileName)
+        {
+            var dateKey = DateTime.ParseExact(Path.GetFileNameWithoutExtension(zipFileName).Split('_')[1], "yyyyMMdd",
+                CultureInfo.InvariantCulture);
+            var data = new Dictionary<string, DbItem>();
+            using (var zip = ZipFile.Open(zipFileName, ZipArchiveMode.Read))
+                foreach (var entry in zip.Entries.Where(a => a.Length > 0))
+                {
+                    var oo = ZipUtils.DeserializeZipEntry<cRoot>(entry);
+                    foreach (var item in oo.result)
+                    {
+                        var dbItem = new DbItem()
+                        {
+                            Symbol = item.ticker,
+                            Date = dateKey,
+                            Exchange = item.exchange.shortName,
+                            Sector = item.gicsSubIndustry?.sector.name,
+                            Industry = item.gicsSubIndustry?.name,
+                            Name = string.IsNullOrEmpty(item.name) ? null: item.name,
+                            ISIN = string.IsNullOrEmpty(item.isin) ? null : item.isin,
+                            Country = item.country,
+                            Type = item.type,
+                            Etf = item.etf ? true : null
+                        };
+                        data.Add(dbItem.Symbol, dbItem);
+                    }
+                }
+
+            DbUtils.ClearAndSaveToDbTable(data.Values, "dbQ2023Others..Bfr_ScreenerChartmill", "Symbol", "Date",
+                "Exchange", "Sector", "Industry", "Name", "ISIN", "Country", "Type", "Etf");
+
+            DbUtils.RunProcedure("dbQ2023Others..pUpdateScreenerChartmill");
+        }
+
+        private static string DownloadAndSaveToZip(DateTime? date)
+        {
+            var dateKey = (date ?? DateTime.Now.AddHours(-8).Date).ToString("yyyyMMdd");
+            var virtualFileEntries = new List<VirtualFileEntry>();
+            var zipFileName = Path.Combine(DataFolder, $"Chartmill_{dateKey}.zip");
+            var entryNameTemplate = Path.Combine(Path.GetFileNameWithoutExtension(zipFileName), $"Chartmill_{dateKey}_" + "{0}.json");
+
+            var jsonFolder = Path.Combine(JsonDataFolder, $@"Chartmill_{dateKey}");
+            if (!Directory.Exists(jsonFolder))
+                Directory.CreateDirectory(jsonFolder);
+
+            var from = 0;
+            var rows = 0;
+            while (from == 0 || from < rows)
+            {
+                var filename = Path.Combine(jsonFolder, $@"Chartmill_{dateKey}_{from}.json");
+                byte[] bytes;
+                if (!File.Exists(filename))
+                {
+                    var parameters = date.HasValue
+                        ? ParameterTemplateWithDate.Replace("{0}", from.ToString())
+                            .Replace("{1}", date.Value.ToString("yyyy-MM-dd"))
+                        : ParameterTemplate.Replace("{0}", from.ToString());
+
+                    var o = Download.PostToBytes(Url, parameters, false);
+                    if (o is Exception ex)
+                        throw new Exception(
+                            $"ChartmillScreenerLoader: Error while download from {Url}. Error message: {ex.Message}");
+                    bytes = (byte[])o;
+                    File.WriteAllBytes(filename, bytes);
+                }
+                else
+                    bytes = File.ReadAllBytes(filename);
+
+                var entryName = string.Format(entryNameTemplate, from.ToString());
+                var entry = new VirtualFileEntry(entryName, bytes);
+                virtualFileEntries.Add(entry);
+
+                var oo = SpanJson.JsonSerializer.Generic.Utf8.Deserialize<cRoot>(bytes);
+                rows = oo.total;
+                from = oo.from + oo.result.Length;
+
+                if (rows == 0)
+                    throw new Exception("ChartmillScreenerLoader. No data downloaded");
+            }
+
+            ZipUtils.ZipVirtualFileEntries(zipFileName, virtualFileEntries);
+            return zipFileName;
+        }
+
+
+        #region =========  SubClasses  ===========
+        public class DbItem
+        {
+            public string Symbol;
+            public DateTime Date;
+            public string Exchange;
+            public string Sector;
+            public string Industry;
+            public string Name;
+            public string ISIN;
+            public byte? Country;
+            public byte? Type;
+            public bool? Etf;
+
+            //   public DateTime Date => DateTime.ParseExact(sDate, "yyyyMMddHHmmss", CultureInfo.InvariantCulture).Date;
+            // public DateTime LastUpdated => DateTime.ParseExact(sLastUpdated, "yyyyMMddHHmmss", CultureInfo.InvariantCulture).Date;
+        }
+
+        private class cRoot
+        {
+            public int total;
+            public int from;
+            public string error;
+            public cResult[] result;
+        }
+
+        private class cResult
+        {
+            public int id;
+            public string isin;
+            public cExchange exchange;
+            public string ticker;
+            public string name;
+            public byte? country;
+            public cGicsSubIndustry gicsSubIndustry;
+            public bool etf;
+            public byte? type; // ?1-ADR, 3-
+        }
+
+        private class cExchange
+        {
+            public string shortName;
+        }
+
+        public class cGicsSubIndustry
+        {
+            public string name;
+            public cSector sector;
+        }
+        public class cSector
+        {
+            public string name;
+        }
+        #endregion
+
+    }
+}
