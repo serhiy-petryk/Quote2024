ee0d2910bad15422da0330022dac35012f1f1cf3
serhiy-petryk <sergei_petrik@yahoo.com>
2024-03-20 23:57:44 +0200

    Update Logger.cs

--- Quote2024/Data/Helpers/Logger.cs
+++ Quote2024/Data/Helpers/Logger.cs
@@ -6,18 +6,20 @@ namespace Data.Helpers
 {
     public static class Logger
     {
+        public enum Application {Main, RealTime};
+
         public delegate void MessageAddedEventHandler(object sender, MessageAddedEventArgs e);
         public static event MessageAddedEventHandler MessageAdded;
 
         private static readonly ConcurrentBag<MessageAddedEventArgs> Messages = new ConcurrentBag<MessageAddedEventArgs>();
 
-        public static void AddMessage(string message)
+        public static void AddMessage(string message, Application application = Application.Main)
         {
             var sf = new StackFrame(1);
             var method = sf.GetMethod();
             var callMethodName = method.DeclaringType?.Name + "." + method.Name;
 
-            var oMessage = new MessageAddedEventArgs(message, callMethodName);
+            var oMessage = new MessageAddedEventArgs(message, callMethodName, application);
             Messages.Add(oMessage);
             MessageAdded?.Invoke(null, oMessage);
         }
@@ -27,10 +29,12 @@ namespace Data.Helpers
             public readonly DateTime Date = DateTime.Now;
             public readonly string MethodName;
             public readonly string Message;
+            public readonly Application Application;
             public string FullMessage => $"{MethodName}. {Message}";
-            public MessageAddedEventArgs(string msg, string methodName)
+            public MessageAddedEventArgs(string msg, string methodName, Application application)
             {
                 Message = msg;
+                Application = application;
                 MethodName = methodName;
                 if (Message.StartsWith("!"))
                     Debug.Print($"LOGGER: {FullMessage}");
