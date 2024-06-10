23fbba597b30e9599e212951f0d367383abf5307
serhiy-petryk <sergei_petrik@yahoo.com>
2024-06-10 23:54:35 +0300

    Update WebClientExt.cs

--- Quote2024/Data/Helpers/WebClientExt.cs
+++ Quote2024/Data/Helpers/WebClientExt.cs
@@ -7,6 +7,8 @@ namespace Data.Helpers
 {
     public class WebClientExt : WebClient
     {
+        private enum Method {Get, Post}
+
         #region ====  Static region  ====
         public static Task<byte[]> DownloadToBytesAsync(string url, bool isXmlHttpRequest = false, bool noProxy = false)
         {
@@ -21,37 +23,15 @@ namespace Data.Helpers
             }
         }
 
-        public static (byte[], CookieCollection, Exception) GetToBytes(string url, bool isJson, bool isXmlHttpRequest = false, CookieCollection cookies = null)
-        {
-            using (var wc = new WebClientExt())
-            {
-                wc.Encoding = System.Text.Encoding.UTF8;
-                wc._isXmlHttpRequest = isXmlHttpRequest;
-                wc.Headers.Add(HttpRequestHeader.Referer, new Uri(url).Host);
-                wc._cookies = new CookieContainer();
-                wc._cookies.Add(cookies ?? new CookieCollection());
+        public static (byte[], CookieCollection, Exception) GetToBytes(string url, bool isJson,
+            bool isXmlHttpRequest = false, CookieCollection cookies = null) =>
+            DoRequest(Method.Get, url, null, isJson, isXmlHttpRequest, null, cookies);
 
-                try
-                {
-                    var response = wc.DownloadData(url);
-                    if (isJson && !IsJsonFormat(response))
-                        throw new Exception($"Downloaded content is not in JSON format");
-                    return (response, wc._responseCookies, null);
-                }
-                catch (Exception ex)
-                {
-                    if (ex is WebException)
-                    {
-                        Debug.Print($"{DateTime.Now}. Web Exception: {url}. Message: {ex.Message}");
-                        return (null, wc._responseCookies, ex);
-                    }
-                    else
-                        throw ex;
-                }
-            }
-        }
+        public static (byte[], CookieCollection, Exception) PostToBytes(string url, string parameters, bool isJson,
+            bool isXmlHttpRequest = false, string contentType = null, CookieCollection cookies = null) =>
+            DoRequest(Method.Post, url, parameters, isJson, isXmlHttpRequest, contentType, cookies);
 
-        public static (byte[], CookieCollection, Exception) PostToBytes(string url, string parameters, bool isJson, bool isXmlHttpRequest = false, string contentType = null, CookieCollection cookies = null)
+        private static (byte[], CookieCollection, Exception) DoRequest(Method method, string url, string postParameters, bool isJson, bool isXmlHttpRequest = false, string contentType = null, CookieCollection cookies = null)
         {
             // see https://stackoverflow.com/questions/5401501/how-to-post-data-to-specific-url-using-webclient-in-c-sharp
             using (var wc = new WebClientExt())
@@ -59,13 +39,16 @@ namespace Data.Helpers
                 wc.Encoding = System.Text.Encoding.UTF8;
                 wc._isXmlHttpRequest = isXmlHttpRequest;
                 wc.Headers.Add(HttpRequestHeader.Referer, new Uri(url).Host);
-                wc.Headers.Add(HttpRequestHeader.ContentType, contentType ?? "application/x-www-form-urlencoded"); // very important for Investing.Splits
+                if (method == Method.Post)
+                    wc.Headers.Add(HttpRequestHeader.ContentType, contentType ?? "application/x-www-form-urlencoded"); // very important for Investing.Splits
                 wc._cookies = new CookieContainer();
                 wc._cookies.Add(cookies ?? new CookieCollection());
 
                 try
                 {
-                    var response = wc.UploadData(url, "POST", wc.Encoding.GetBytes(parameters));
+                    var response = method == Method.Post
+                        ? wc.UploadData(url, "POST", wc.Encoding.GetBytes(postParameters))
+                        : wc.DownloadData(url);
                     if (isJson && (response.Length < 2 || response[0] != 0x7b || response[response.Length - 1] != 0x7d))
                         throw new Exception($"Downloaded content is not in JSON format");
                     return (response, wc._responseCookies, null);
@@ -82,10 +65,6 @@ namespace Data.Helpers
                 }
             }
         }
-
-        private static bool IsJsonFormat(byte[] response) => response.Length > 1 &&
-            ((response[0] == '{' && response[response.Length - 1] == '}') ||
-             (response[0] == '[' && response[response.Length - 1] == ']'));
         #endregion
 
         #region ====  Instance region  =====
