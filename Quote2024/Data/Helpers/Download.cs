f3505f66494134b9d8a05625ffc332ae5d411f1d
serhiy-petryk <sergei_petrik@yahoo.com>
2024-06-10 23:34:41 +0300

    Update Download.cs

--- Quote2024/Data/Helpers/Download.cs
+++ Quote2024/Data/Helpers/Download.cs
@@ -5,16 +5,17 @@ using System.Threading.Tasks;
 
 namespace Data.Helpers
 {
-    public static class Download
+    public class Download : WebClient
     {
+        #region ====  Static region  ====
         public static Task<byte[]> DownloadToBytesAsync(string url, bool isXmlHttpRequest = false, bool noProxy = false)
         {
-            using (var wc = new WebClientEx())
+            using (var wc = new Download())
             {
                 if (noProxy) wc.Proxy = null;
                 wc.Encoding = System.Text.Encoding.UTF8;
                 // wc.Cookies = cookies;
-                wc.IsXmlHttpRequest = isXmlHttpRequest;
+                wc._isXmlHttpRequest = isXmlHttpRequest;
                 wc.Headers.Add(HttpRequestHeader.Referer, new Uri(url).Host);
                 return wc.DownloadDataTaskAsync(url);
             }
@@ -22,27 +23,27 @@ namespace Data.Helpers
 
         public static (byte[], CookieCollection, Exception) GetToBytes(string url, bool isJson, bool isXmlHttpRequest = false, CookieCollection cookies = null)
         {
-            using (var wc = new WebClientEx())
+            using (var wc = new Download())
             {
                 wc.Encoding = System.Text.Encoding.UTF8;
-                wc.IsXmlHttpRequest = isXmlHttpRequest;
+                wc._isXmlHttpRequest = isXmlHttpRequest;
                 wc.Headers.Add(HttpRequestHeader.Referer, new Uri(url).Host);
-                wc.Cookies = new CookieContainer();
-                wc.Cookies.Add(cookies ?? new CookieCollection());
+                wc._cookies = new CookieContainer();
+                wc._cookies.Add(cookies ?? new CookieCollection());
 
                 try
                 {
                     var response = wc.DownloadData(url);
                     if (isJson && !IsJsonFormat(response))
                         throw new Exception($"Downloaded content is not in JSON format");
-                    return (response, wc.ResponseCookies, null);
+                    return (response, wc._responseCookies, null);
                 }
                 catch (Exception ex)
                 {
                     if (ex is WebException)
                     {
                         Debug.Print($"{DateTime.Now}. Web Exception: {url}. Message: {ex.Message}");
-                        return (null, wc.ResponseCookies, ex);
+                        return (null, wc._responseCookies, ex);
                     }
                     else
                         throw ex;
@@ -53,28 +54,28 @@ namespace Data.Helpers
         public static (byte[], CookieCollection, Exception) PostToBytes(string url, string parameters, bool isJson, bool isXmlHttpRequest = false, string contentType = null, CookieCollection cookies = null)
         {
             // see https://stackoverflow.com/questions/5401501/how-to-post-data-to-specific-url-using-webclient-in-c-sharp
-            using (var wc = new WebClientEx())
+            using (var wc = new Download())
             {
                 wc.Encoding = System.Text.Encoding.UTF8;
-                wc.IsXmlHttpRequest = isXmlHttpRequest;
+                wc._isXmlHttpRequest = isXmlHttpRequest;
                 wc.Headers.Add(HttpRequestHeader.Referer, new Uri(url).Host);
                 wc.Headers.Add(HttpRequestHeader.ContentType, contentType ?? "application/x-www-form-urlencoded"); // very important for Investing.Splits
-                wc.Cookies = new CookieContainer();
-                wc.Cookies.Add(cookies ?? new CookieCollection());
+                wc._cookies = new CookieContainer();
+                wc._cookies.Add(cookies ?? new CookieCollection());
 
                 try
                 {
                     var response = wc.UploadData(url, "POST", wc.Encoding.GetBytes(parameters));
                     if (isJson && (response.Length < 2 || response[0] != 0x7b || response[response.Length - 1] != 0x7d))
                         throw new Exception($"Downloaded content is not in JSON format");
-                    return (response, wc.ResponseCookies, null);
+                    return (response, wc._responseCookies, null);
                 }
                 catch (Exception ex)
                 {
                     if (ex is WebException)
                     {
                         Debug.Print($"{DateTime.Now}. Web Exception: {url}. Message: {ex.Message}");
-                        return (null, wc.ResponseCookies, ex);
+                        return (null, wc._responseCookies, ex);
                     }
                     else
                         throw ex;
@@ -85,44 +86,45 @@ namespace Data.Helpers
         private static bool IsJsonFormat(byte[] response) => response.Length > 1 &&
             ((response[0] == '{' && response[response.Length - 1] == '}') ||
              (response[0] == '[' && response[response.Length - 1] == ']'));
+        #endregion
 
-        public class WebClientEx : WebClient
+        #region ====  Instance region  =====
+
+        private int? _timeoutInMilliseconds;
+        private CookieContainer _cookies;
+        private bool _isXmlHttpRequest;
+        private CookieCollection _responseCookies;
+
+        protected override WebResponse GetWebResponse(WebRequest request)
         {
-            public int? TimeoutInMilliseconds;
-            public CookieContainer Cookies;
-            public bool IsXmlHttpRequest;
-            public CookieCollection ResponseCookies;
-
-            protected override WebResponse GetWebResponse(WebRequest request)
-            {
-                var response = base.GetWebResponse(request);
-                ResponseCookies = ((HttpWebResponse)response).Cookies;
-                return response;
-            }
-
-            protected override WebRequest GetWebRequest(Uri address)
-            {
-                var request = (HttpWebRequest)base.GetWebRequest(address);
-                request.UserAgent = @"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36";
-                request.AllowAutoRedirect = true;
-                request.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip, deflate");
-                request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-
-                /*request.ContentType = "application/json";
-                request.MediaType = "application/json";
-                request.Accept = "application/json";
-                request.Method = "POST";*/
-
-                if (IsXmlHttpRequest)
-                    request.Headers.Add("X-Requested-With", "XMLHttpRequest");
-
-                if (Cookies != null)
-                    request.CookieContainer = Cookies;
-
-                if (TimeoutInMilliseconds.HasValue)
-                    request.Timeout = TimeoutInMilliseconds.Value;
-                return request;
-            }
+            var response = base.GetWebResponse(request);
+            _responseCookies = ((HttpWebResponse)response).Cookies;
+            return response;
         }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            var request = (HttpWebRequest)base.GetWebRequest(address);
+            request.UserAgent = @"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36";
+            request.AllowAutoRedirect = true;
+            request.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip, deflate");
+            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+            /*request.ContentType = "application/json";
+            request.MediaType = "application/json";
+            request.Accept = "application/json";
+            request.Method = "POST";*/
+
+            if (_isXmlHttpRequest)
+                request.Headers.Add("X-Requested-With", "XMLHttpRequest");
+
+            if (_cookies != null)
+                request.CookieContainer = _cookies;
+
+            if (_timeoutInMilliseconds.HasValue)
+                request.Timeout = _timeoutInMilliseconds.Value;
+            return request;
+        }
+        #endregion
     }
 }
