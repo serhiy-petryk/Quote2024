d6670ebdd351ac5ee508b0a6c22246fbb53007de
serhiy-petryk <sergei_petrik@yahoo.com>
2024-06-19 19:24:35 +0300

    x

--- Quote2024/Data/Actions/MorningStar/MorningStarSymbolsLoader.cs
+++ Quote2024/Data/Actions/MorningStar/MorningStarSymbolsLoader.cs
@@ -5,7 +5,6 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Text;
 using System.Threading.Tasks;
 using Data.Helpers;
 using Microsoft.Data.SqlClient;
