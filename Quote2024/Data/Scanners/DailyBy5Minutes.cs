29d70fbbe6d23b2822cc7010c9d84e58416d1670
serhiy-petryk <sergei_petrik@yahoo.com>
2024-10-29 06:25:23 +0200

    Update DailyBy5Minutes.cs

--- Quote2024/Data/Scanners/DailyBy5Minutes.cs
+++ Quote2024/Data/Scanners/DailyBy5Minutes.cs
@@ -118,22 +118,22 @@ namespace Data.Scanners
         {
             Logger.AddMessage($"Save data to database ...");
             DbHelper.SaveToDbTable(data, "dbQ2024MinuteScanner..DailyBy5Minutes", "Symbol", "PrevDate", "NextDate",
-                "O0930", "H0930", "L0930", "C0930", "HG0930", "LG0930",
-                "O0931", "H0931", "L0931", "C0931", "HG0931", "LG0931",
-                "O0932", "H0932", "L0932", "C0932", "HG0932", "LG0932",
-                "O0933", "H0933", "L0933", "C0933", "HG0933", "LG0933",
-                "O0934", "H0934", "L0934", "C0934", "HG0934", "LG0934",
-                "O0935", "H0935", "L0935", "C0935", "HG0935", "LG0935",
-                "O0940", "H0940", "L0940", "C0940", "HG0940", "LG0940",
-                "O0945", "H0945", "L0945", "C0945", "HG0945", "LG0945",
-                "O0950", "H0950", "L0950", "C0950", "HG0950", "LG0950",
-                "O0955", "H0955", "L0955", "C0955", "HG0955", "LG0955",
-                "O1000", "H1000", "L1000", "C1000", "HG1000", "LG1000",
-                "O1005", "H1005", "L1005", "C1005", "HG1005", "LG1005",
-                "O1010", "H1010", "L1010", "C1010", "HG1010", "LG1010",
-                "O1015", "H1015", "L1015", "C1015", "HG1015", "LG1015",
-                "O1020", "H1020", "L1020", "C1020", "HG1020", "LG1020",
-                "O1025", "H1025", "L1025", "C1025", "HG1025", "LG1025",
+                "O0930", "H0930", "L0930", "C0930", "V0930", "N0930", "HG0930", "LG0930",
+                "O0931", "H0931", "L0931", "C0931", "V0931", "N0931", "HG0931", "LG0931",
+                "O0932", "H0932", "L0932", "C0932", "V0932", "N0932", "HG0932", "LG0932",
+                "O0933", "H0933", "L0933", "C0933", "V0933", "N0933", "HG0933", "LG0933",
+                "O0934", "H0934", "L0934", "C0934", "V0934", "N0934", "HG0934", "LG0934",
+                "O0935", "H0935", "L0935", "C0935", "V0935", "N0935", "HG0935", "LG0935",
+                "O0940", "H0940", "L0940", "C0940", "V0940", "N0940", "HG0940", "LG0940",
+                "O0945", "H0945", "L0945", "C0945", "V0945", "N0945", "HG0945", "LG0945",
+                "O0950", "H0950", "L0950", "C0950", "V0950", "N0950", "HG0950", "LG0950",
+                "O0955", "H0955", "L0955", "C0955", "V0955", "N0955", "HG0955", "LG0955",
+                "O1000", "H1000", "L1000", "C1000", "V1000", "N1000", "HG1000", "LG1000",
+                "O1005", "H1005", "L1005", "C1005", "V1000", "N1005", "HG1005", "LG1005",
+                "O1010", "H1010", "L1010", "C1010", "V1010", "N1010", "HG1010", "LG1010",
+                "O1015", "H1015", "L1015", "C1015", "V1015", "N1015", "HG1015", "LG1015",
+                "O1020", "H1020", "L1020", "C1020", "V1020", "N1020", "HG1020", "LG1020",
+                "O1025", "H1025", "L1025", "C1025", "V1025", "N1025", "HG1025", "LG1025",
                 "O1520", "H1520", "L1520", "O1525", "H1525", "L1525",
                 "O1530", "H1530", "L1530", "O1535", "H1535", "L1535",
                 "O1540", "H1540", "L1540", "O1545", "H1545", "L1545",
@@ -153,6 +153,8 @@ namespace Data.Scanners
             public float? C0930 => Intervals[0].Close;
             public float? HG0930 => Intervals[0].HighGlobal;
             public float? LG0930 => Intervals[0].LowGlobal;
+            public float V0930 => Intervals[0].Volume;
+            public int N0930 => Intervals[0].Trades;
 
             public float? O0931 => Intervals[21].Open;
             public float? H0931 => Intervals[21].High;
@@ -160,6 +162,8 @@ namespace Data.Scanners
             public float? C0931 => Intervals[21].Close;
             public float? HG0931 => Intervals[21].HighGlobal;
             public float? LG0931 => Intervals[21].LowGlobal;
+            public float V0931 => Intervals[21].Volume;
+            public int N0931 => Intervals[21].Trades;
 
             public float? O0932 => Intervals[22].Open;
             public float? H0932 => Intervals[22].High;
@@ -167,6 +171,8 @@ namespace Data.Scanners
             public float? C0932 => Intervals[22].Close;
             public float? HG0932 => Intervals[22].HighGlobal;
             public float? LG0932 => Intervals[22].LowGlobal;
+            public float V0932 => Intervals[22].Volume;
+            public int N0932 => Intervals[22].Trades;
 
             public float? O0933 => Intervals[23].Open;
             public float? H0933 => Intervals[23].High;
@@ -174,6 +180,8 @@ namespace Data.Scanners
             public float? C0933 => Intervals[23].Close;
             public float? HG0933 => Intervals[23].HighGlobal;
             public float? LG0933 => Intervals[23].LowGlobal;
+            public float V0933 => Intervals[23].Volume;
+            public int N0933 => Intervals[23].Trades;
 
             public float? O0934 => Intervals[24].Open;
             public float? H0934 => Intervals[24].High;
@@ -181,6 +189,8 @@ namespace Data.Scanners
             public float? C0934 => Intervals[24].Close;
             public float? HG0934 => Intervals[24].HighGlobal;
             public float? LG0934 => Intervals[24].LowGlobal;
+            public float V0934 => Intervals[24].Volume;
+            public int N0934 => Intervals[24].Trades;
 
             public float? O0935 => Intervals[1].Open;
             public float? H0935 => Intervals[1].High;
@@ -188,6 +198,8 @@ namespace Data.Scanners
             public float? C0935 => Intervals[1].Close;
             public float? HG0935 => Intervals[1].HighGlobal;
             public float? LG0935 => Intervals[1].LowGlobal;
+            public float V0935 => Intervals[1].Volume;
+            public int N0935 => Intervals[1].Trades;
 
             public float? O0940 => Intervals[2].Open;
             public float? H0940 => Intervals[2].High;
@@ -195,6 +207,8 @@ namespace Data.Scanners
             public float? C0940 => Intervals[2].Close;
             public float? HG0940 => Intervals[2].HighGlobal;
             public float? LG0940 => Intervals[2].LowGlobal;
+            public float V0940 => Intervals[2].Volume;
+            public int N0940 => Intervals[2].Trades;
 
             public float? O0945 => Intervals[3].Open;
             public float? H0945 => Intervals[3].High;
@@ -202,6 +216,8 @@ namespace Data.Scanners
             public float? C0945 => Intervals[3].Close;
             public float? HG0945 => Intervals[3].HighGlobal;
             public float? LG0945 => Intervals[3].LowGlobal;
+            public float V0945 => Intervals[3].Volume;
+            public int N0945 => Intervals[3].Trades;
 
             public float? O0950 => Intervals[4].Open;
             public float? H0950 => Intervals[4].High;
@@ -209,6 +225,8 @@ namespace Data.Scanners
             public float? C0950 => Intervals[4].Close;
             public float? HG0950 => Intervals[4].HighGlobal;
             public float? LG0950 => Intervals[4].LowGlobal;
+            public float V0950 => Intervals[4].Volume;
+            public int N0950 => Intervals[4].Trades;
 
             public float? O0955 => Intervals[5].Open;
             public float? H0955 => Intervals[5].High;
@@ -216,6 +234,8 @@ namespace Data.Scanners
             public float? C0955 => Intervals[5].Close;
             public float? HG0955 => Intervals[5].HighGlobal;
             public float? LG0955 => Intervals[5].LowGlobal;
+            public float V0955 => Intervals[5].Volume;
+            public int N0955 => Intervals[5].Trades;
 
             public float? O1000 => Intervals[6].Open;
             public float? H1000 => Intervals[6].High;
@@ -223,6 +243,8 @@ namespace Data.Scanners
             public float? C1000 => Intervals[6].Close;
             public float? HG1000 => Intervals[6].HighGlobal;
             public float? LG1000 => Intervals[6].LowGlobal;
+            public float V1000 => Intervals[6].Volume;
+            public int N1000 => Intervals[6].Trades;
 
             public float? O1005 => Intervals[7].Open;
             public float? H1005 => Intervals[7].High;
@@ -230,6 +252,8 @@ namespace Data.Scanners
             public float? C1005 => Intervals[7].Close;
             public float? HG1005 => Intervals[7].HighGlobal;
             public float? LG1005 => Intervals[7].LowGlobal;
+            public float V1005 => Intervals[7].Volume;
+            public int N1005 => Intervals[7].Trades;
 
             public float? O1010 => Intervals[8].Open;
             public float? H1010 => Intervals[8].High;
@@ -237,6 +261,8 @@ namespace Data.Scanners
             public float? C1010 => Intervals[8].Close;
             public float? HG1010 => Intervals[8].HighGlobal;
             public float? LG1010 => Intervals[8].LowGlobal;
+            public float V1010 => Intervals[8].Volume;
+            public int N1010 => Intervals[8].Trades;
 
             public float? O1015 => Intervals[9].Open;
             public float? H1015 => Intervals[9].High;
@@ -244,6 +270,8 @@ namespace Data.Scanners
             public float? C1015 => Intervals[9].Close;
             public float? HG1015 => Intervals[9].HighGlobal;
             public float? LG1015 => Intervals[9].LowGlobal;
+            public float V1015 => Intervals[9].Volume;
+            public int N1015 => Intervals[9].Trades;
 
             public float? O1020 => Intervals[10].Open;
             public float? H1020 => Intervals[10].High;
@@ -251,6 +279,8 @@ namespace Data.Scanners
             public float? C1020 => Intervals[10].Close;
             public float? HG1020 => Intervals[10].HighGlobal;
             public float? LG1020 => Intervals[10].LowGlobal;
+            public float V1020 => Intervals[10].Volume;
+            public int N1020 => Intervals[10].Trades;
 
             public float? O1025 => Intervals[11].Open;
             public float? H1025 => Intervals[11].High;
@@ -258,6 +288,8 @@ namespace Data.Scanners
             public float? C1025 => Intervals[11].Close;
             public float? HG1025 => Intervals[11].HighGlobal;
             public float? LG1025 => Intervals[11].LowGlobal;
+            public float V1025 => Intervals[11].Volume;
+            public int N1025 => Intervals[11].Trades;
 
             // =================
             public float? O1520 => Intervals[12].Open;
@@ -327,6 +359,8 @@ namespace Data.Scanners
             public float? High;
             public float? Low;
             public float? Close;
+            public float Volume;
+            public int Trades;
             public float? HighGlobal;
             public float? LowGlobal;
 
@@ -350,6 +384,9 @@ namespace Data.Scanners
             {
                 if (item.DateTime.TimeOfDay < ToLocal)
                 {
+                    Volume += item.v;
+                    Trades += item.n;
+
                     if (Open.HasValue)
                     {
                         if (High < item.h) High = item.h;
