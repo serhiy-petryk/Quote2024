6b7efffc927683a9da29b7ca36bfc76bf186fcd8
serhiy-petryk <sergei_petrik@yahoo.com>
2025-08-11 22:03:32 +0300

    Update EoddataCommon.cs

--- Quote2024/Data/Actions/Eoddata/EoddataCommon.cs
+++ Quote2024/Data/Actions/Eoddata/EoddataCommon.cs
@@ -61,55 +61,5 @@ namespace Data.Actions.Eoddata
 
             return _eoddataCookies;
         }
-
-        public static CookieCollection GetEoddataCookies_before_2025_08_08()
-        {
-            if (_eoddataCookies == null)
-            {
-                var userAndPassword = Data.Helpers.CsUtils.GetApiKeys("eoddata.com")[0].Split('^');
-                var cookies = new NameValueCollection
-                {
-                    { "ctl00$Menu1$s1$txtSearch", "" },
-                    { "ctl00$cph1$lg1$txtEmail", userAndPassword[0] },
-                    { "ctl00$cph1$lg1$txtPassword", userAndPassword[1] },
-                    { "ctl00$cph1$lg1$chkRemember", "off" },
-                    { "ctl00$cph1$lg1$btnLogin", "Login" }
-                };
-
-                var urlOld = @"https://www.eoddata.com";
-                var o1 = WebClientExt.GetToBytes(urlOld, false, false, null);
-                var content = System.Text.Encoding.UTF8.GetString(o1.Item1);
-                var ss = content.Split("input type=\"hidden\"");
-                for (var k = 1; k < ss.Length; k++)
-                {
-                    var s = ss[k].Substring(0, ss[k].IndexOf("/>", StringComparison.InvariantCulture));
-                    var k1 = s.IndexOf("name=\"", StringComparison.InvariantCulture);
-                    var k2 = s.IndexOf("\"", k1 + 6, StringComparison.InvariantCulture);
-                    var name = s.Substring(k1 + 6, k2 - k1 - 6);
-                    k1 = s.IndexOf("value=\"", StringComparison.InvariantCulture);
-                    k2 = s.IndexOf("\"", k1 + 7, StringComparison.InvariantCulture);
-                    var value = s.Substring(k1 + 7, k2 - k1 - 7);
-                    cookies.Add(name, value);
-                }
-
-                var parameters = string.Join("&",
-                    cookies.AllKeys.Select(a =>
-                        System.Net.WebUtility.UrlEncode(a) + "=" + System.Net.WebUtility.UrlEncode(cookies[a])));
-
-                var o2 = WebClientExt.PostToBytes(urlOld, parameters, false, false, "application/x-www-form-urlencoded",
-                    o1.Item2);
-                _eoddataCookies = o2.Item2;
-
-                if (_eoddataCookies["ASP.NET_SessionId"] == null)
-                    throw new Exception("No 'ASP.NET_SessionId' cookie");
-                //if (_eoddataCookies["EODDataAdmin"] == null)
-                //  throw new Exception("No 'EODDataAdmin' cookie");
-                //if (_eoddataCookies["EODDataLogin"] == null)
-                //  throw new Exception("No 'EODDataLogin' cookie");
-            }
-
-            return _eoddataCookies;
-        }
-
     }
 }
