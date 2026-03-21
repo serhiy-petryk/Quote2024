5b201247a9c59400f953af19ae771ab1b0090cb8
serhiy-petryk <sergei_petrik@yahoo.com>
2026-03-21 02:30:39 +0200

    Update MainForm.Designer.cs

--- Quote2024/Quote2024/Forms/MainForm.Designer.cs
+++ Quote2024/Quote2024/Forms/MainForm.Designer.cs
@@ -200,7 +200,7 @@ namespace Quote2024.Forms
             // btnOpenTestForm
             // 
             this.btnOpenTestForm.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnOpenTestForm.Location = new System.Drawing.Point(580, 310);
+            this.btnOpenTestForm.Location = new System.Drawing.Point(580, 315);
             this.btnOpenTestForm.Name = "btnOpenTestForm";
             this.btnOpenTestForm.Size = new System.Drawing.Size(186, 33);
             this.btnOpenTestForm.TabIndex = 75;
@@ -385,7 +385,7 @@ namespace Quote2024.Forms
             this.tabPage2.Location = new System.Drawing.Point(4, 24);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(970, 355);
+            this.tabPage2.Size = new System.Drawing.Size(951, 355);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "tabPage2";
             this.tabPage2.UseVisualStyleBackColor = true;
